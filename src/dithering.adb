14329bc60b9177d23798c4242cbc22d17a6c70f5
Pierre-Marie de Rodat <pmderodat@kawie.fr>
2016-12-26 19:08:49 +0100

    Refactor the main to use Resource_Holders for file handles

--- src/dithering.adb
+++ src/dithering.adb
@@ -6,6 +6,7 @@ with HAL.Filesystem;    use HAL.Filesystem;
 with Native.Filesystem; use Native.Filesystem;
 
 with BMP;
+with Resource_Holders;
 
 procedure Dithering is
 
@@ -17,6 +18,11 @@ procedure Dithering is
    --  This runs the whole program
 
    procedure Process_File (Read_Handle, Write_Handle : Any_File_Handle);
+
+   procedure Close (Handle : in out Any_File_Handle);
+
+   package File_Handle_Holders is new Resource_Holders
+     (Any_File_Handle, Close);
 
    ----------
    -- Main --
@@ -40,30 +46,25 @@ procedure Dithering is
       declare
          Read_Filename             : constant String := Argument (1);
          Write_Filename            : constant String := Read_Filename & ".new";
-         Read_Handle, Write_Handle : Any_File_Handle;
+         Read_Handle, Write_Handle : File_Handle_Holders.Holder_Type;
       begin
-         if FS.Open (Read_Filename, Read_Only, Read_Handle) /= Status_Ok then
+         if FS.Open (Read_Filename, Read_Only, Read_Handle.Value) /= Status_Ok
+         then
             Put_Line ("Could not open " & Read_Filename);
             raise Exit_With_Failure;
          end if;
+         File_Handle_Holders.Set_Present (Read_Handle);
+
          if FS.Create_Node (Write_Filename, Regular_File) /= Status_Ok
            or else FS.Open
-             (Write_Filename, Write_Only, Write_Handle) /= Status_Ok
+             (Write_Filename, Write_Only, Write_Handle.Value) /= Status_Ok
          then
             Put_Line ("Could not open " & Write_Filename);
             raise Exit_With_Failure;
          end if;
+         File_Handle_Holders.Set_Present (Write_Handle);
 
-         Process_File (Read_Handle, Write_Handle);
-
-         if Read_Handle.Close /= Status_Ok then
-            Put_Line ("Could not close " & Read_Filename);
-            raise Exit_With_Failure;
-         end if;
-         if Write_Handle.Close /= Status_Ok then
-            Put_Line ("Could not close " & Write_Filename);
-            raise Exit_With_Failure;
-         end if;
+         Process_File (Read_Handle.Value, Write_Handle.Value);
       end;
    end Main;
 
@@ -77,6 +78,18 @@ procedure Dithering is
       BMP.Save (Write_Handle.all, BM.Bitmap);
       BMP.Destroy (BM);
    end Process_File;
+
+   -----------
+   -- Close --
+   -----------
+
+   procedure Close (Handle : in out Any_File_Handle) is
+   begin
+      if Handle.Close /= Status_Ok then
+         Put_Line ("Could not close a file");
+      end if;
+      Handle := null;
+   end Close;
 
 begin
    Main;
